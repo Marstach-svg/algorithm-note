f9b8e74ead9643414254bb4a405c2c1f5f02d4c2
Marstach <ryoken.0325@gmail.com>
2025-10-29 19:24:29 +0900

    atcoder-abc-391-B

---
+++ abc/abc-391- B.py
@@ -0,0 +1,17 @@
+# atcoder 391 B
+# ギブアップ
+# a, bの値を固定して下で調べていくイメージ
+N, M = map(int, input().split())
+S = [input() for _ in range(N)]
+T = [input() for _ in range(M)]
+
+for a in range(N-M+1):
+    for b in range(N-M+1):
+        ok = True
+        for i in range(M):
+            for j in range(M):
+                if S[a + i][b + j] != T[i][j]:
+                    ok = False
+
+        if ok:
+            print(a+1, b+1)
